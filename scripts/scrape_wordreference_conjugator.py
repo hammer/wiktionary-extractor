689a3b0d4e10f1181469c3825af183610cabf014
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-02-20 23:39:57 -0500

    Finish off the WordReference conjugator scraper.

--- scripts/scrape_wordreference_conjugator.py
+++ scripts/scrape_wordreference_conjugator.py
@@ -5,10 +5,18 @@ import re
 import requests
 import sys
 
-XPATH_NO_TENSE = '//td[starts-with(@title, "infinitive")]/following-sibling::td'
-XPATH_PRES = '//th[@title="Indicative"]/../following-sibling::tr[2]/td[2]'
+XPATH_TENSES = {
+  'no_tense': '//td[starts-with(@title, "infinitive")]/following-sibling::td',
+  'pres': '//th[@title="Indicative"]/../following-sibling::tr[2]/td[2]',
+  'imperf': '//th[@title="Indicative"]/../following-sibling::tr[2]/td[3]',
+  'prem': '//th[@title="Indicative"]/../following-sibling::tr[2]/td[4]',
+  'fut': '//th[@title="Indicative"]/../following-sibling::tr[2]/td[5]',
+  'cond': '//th[@title="Conditional"]/../following-sibling::tr[2]/td[2]',
+  'cong': '//th[@title="Subjunctive"]/../following-sibling::tr[2]/td[2]',
+  'congimp': '//th[@title="Subjunctive"]/../following-sibling::tr[2]/td[3]',
+  'imp': '//th[@title="Imperative"]/../following-sibling::tr[2]/td[2]',
+}
 RE_TD = re.compile('<td.*?>(.*?)</td>', re.DOTALL)
-
 WORDREFERENCE_CONJUGATOR_URL_BASE = 'http://www.wordreference.com/conj/ItVerbs.aspx?v='
 
 def get_element_from_xpath(xpath, html_fragment):
@@ -19,6 +27,11 @@ def get_list_from_td_element(td_el):
     innerHTML = RE_TD.search(outerHTML).groups()[0].strip()
     return [word.replace('<b>', '').replace('</b>', '').strip()
             for word in innerHTML.split('<br>')]
+
+def get_conjugated_tense(xpath, html_fragment):
+  td_el = get_element_from_xpath(xpath, verb_html)
+  return get_list_from_td_element(td_el)
+
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Scrape verb conjugations from Wordreference.')
@@ -41,14 +54,12 @@ if __name__ == '__main__':
     r = requests.get(WORDREFERENCE_CONJUGATOR_URL_BASE + verb)
     verb_html = r.content
 
-    # no_tense
-    no_tense_td_el = get_element_from_xpath(XPATH_NO_TENSE, verb_html)
-    logging.info(html.tostring(no_tense_td_el))
-    no_tense = get_list_from_td_element(no_tense_td_el)
-    logging.info(no_tense)
+    conjugated_tenses = []
+    for (tense, xpath) in XPATH_TENSES.items():
+      logging.info('Parsing %s tense.' % tense)
+      conjugated_tense = get_conjugated_tense(xpath, verb_html)
+      logging.info('Conjugated tense: %s' % conjugated_tense)
+      conjugated_tenses.append((tense, conjugated_tense))
 
-    # pres
-    pres_td_el = get_element_from_xpath(XPATH_PRES, verb_html)
-    pres = get_list_from_td_element(pres_td_el)
-    logging.info(pres)
+
 
