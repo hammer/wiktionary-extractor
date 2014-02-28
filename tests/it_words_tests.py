1ccf0baf8a9b0a234de5af502cd0d93ad7c3966e
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-02-28 18:53:56 -0500

    Remove extraneous example functions from tests.

--- tests/it_words_tests.py
+++ tests/it_words_tests.py
@@ -1,11 +1,4 @@
-from nose.tools import *
 from it_words import verb
-
-def setup():
-  print("SETUP!")
-
-def teardown():
-  print("TEAR DOWN!")
 
 def test_irregularity_re():
   verb.IRREGULARITY_RE.match("prem1s").groups() == ('prem', '1', 's', '')
