732291aa5cda41cb50d419f68bb9e10f7447cd52
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-03-03 21:53:49 -0500

    Added another test.

--- tests/it_words_tests.py
+++ tests/it_words_tests.py
@@ -1,5 +1,12 @@
+from nose.tools import *
 from it_words import verb
 
 def test_irregularity_re():
-  verb.IRREGULARITY_RE.match("prem1s").groups() == ('prem', '1', 's', '')
+  assert verb.IRREGULARITY_RE.match("prem1s").groups() == ('prem', '1', 's', '')
+
+@raises(Exception)
+def test_supported_verb_types():
+  verb.Verb('stem', 'type')
+
+
 
