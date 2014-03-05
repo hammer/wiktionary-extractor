2976b2cfb399b145c41c3b6dc02881d050563119
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-03-04 21:56:13 -0500

    Another test.

--- tests/it_words_tests.py
+++ tests/it_words_tests.py
@@ -4,6 +4,10 @@ from it_words import verb
 def test_irregularity_re():
   assert verb.IRREGULARITY_RE.match("prem1s").groups() == ('prem', '1', 's', '')
 
+def test_is_reflexive():
+  assert verb.Verb("alz", "arsi").is_reflexive
+  assert not verb.Verb("alz", "are").is_reflexive
+
 @raises(Exception)
 def test_supported_verb_types():
   verb.Verb('stem', 'type')
